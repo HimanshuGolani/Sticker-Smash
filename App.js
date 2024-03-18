fba02257bbe4bc4631988f548c31ee7accdab236
HimanshuGolani <golanihimanshu@gmail.com>
2024-03-18 10:03:52 +0530

    Committing changes in StickerSmash submodule

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Image } from 'react-native';
+import PreviewImg from "./assets/background-image.png"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <Image source={PreviewImg} style={styles.image}/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imageContainer:{
+    flex:1,
+    paddingTop:58,
+  },
+  image:{
+    width:320,
+    height:440,
+    borderRadius:25,
+  }
 });
